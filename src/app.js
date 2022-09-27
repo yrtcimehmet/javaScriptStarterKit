19b8373ed1fc407390f470a8d26b543cc02a26a4
Mehmet Yirtici <yrtci.mehmet@gmail.com>
2022-09-28 02:27:47 +0300

    18 Ekim Ders Sonu

--- src/app.js
+++ src/app.js
@@ -1,36 +1,30 @@
-console.log("Merhaba Kodlama.io")
+console.log('Merhaba Kodlama.io')
 
-//JS type safe değildir
-let dolarBugun = 9.30
+// JS type safe degildir
+let dolarBugun = 9.30;
 
-let dolarDun = 9.20
-dolarDun = "9.20"
+let dolarDun = 9.20;
 {
-    let dolarDun = 9.10
+    let dolarDun =  9.10
 }
 
 console.log(dolarDun)
 
+
 const euroDun = 11.2
-//euroDun = 11
+//euroDun = 11 
 
 console.log(euroDun)
 
 //array
 //camelCasing
 //PascalCasing
-let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
-
+let konutKredileri = ['Konut kredisi', 'Emlak Konut Kredisi','Kamu Konut Kredisi']
 console.log("<ul>")
-for(let i = 0;i<konutKredileri.length;i++){
+for(let i = 0; i < konutKredileri.length; i++) {
     console.log("<li>"+konutKredileri[i]+"</li>")
 }
-console.log("</ul>")
+console.log("</ul>");
 
-{/* <ul>
-    <li>Konut kredisi</li>
-    <li>Emlak Konut Kredisi</li>
-    <li>Kamu Konut Kredisi</li>
-</ul> */}
 
 console.log(konutKredileri)
